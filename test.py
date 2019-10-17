4d4979835eaabdef35f6e09a995f173f565b28d5
oreodaw <jsmfol2333@hotmail.com>
2019-10-17 20:27:57 +0200

    Comit segunda modificacion

--- test.py
+++ test.py
@@ -13,3 +13,4 @@ else:
             print("Suspendido por ahora o no ")
             print("Eres muy burro")
             print("He modificado el fichero en developer")
+            print("Esto es la segunda modificacion")
